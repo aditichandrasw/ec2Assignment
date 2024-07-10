89c46863199b8b7b59d656312bee44cacd95374b
aditichandrasw <136793732+aditichandrasw@users.noreply.github.com>
2024-07-10 05:10:35 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        Hello!
+        You are connected to EC2
+      </header>
+      <p>By Aditi</p>
+    </div>
+  );
+}
+
+export default App;
